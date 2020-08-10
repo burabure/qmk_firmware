7d7e55ed1aea77a8c5ea311aff95f6586aa68e04
Nicolas BuraBure Fernandez <nicolas@santiagolab.cl>
2020-08-09 20:51:51 -0400

    scroll left hand

--- keyboards/dactyl_manuform/keymaps/default/keymap.c
+++ keyboards/dactyl_manuform/keymaps/default/keymap.c
@@ -66,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [ARKY] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,  \
-        _______, _______, _______, KC_UP,   KC_PGUP, KC_HOME,                                      KC_HOME, KC_PGUP, KC_UP,   _______, _______,    _______, \
+        _______, _______, KC_WH_U, KC_UP,   KC_PGUP, KC_HOME,                                      KC_HOME, KC_PGUP, KC_UP,   _______, _______,    _______, \
         _______,C(KC_LEFT),KC_LEFT,KC_DOWN, KC_RGHT, C(KC_RGHT),                                C(KC_LEFT), KC_LEFT, KC_DOWN, KC_RGHT, C(KC_RGHT), _______, \
-        _______, _______, _______, _______, KC_PGDN, _______,                                      _______, KC_PGDN, _______, _______, _______,    _______, \
+        _______, _______, KC_WH_D, _______, KC_PGDN, _______,                                      _______, KC_PGDN, _______, _______, _______,    _______, \
                           SW_DE_B, SW_DE_F, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, \
                                                               _______, _______,  _______, _______ \
     ),
